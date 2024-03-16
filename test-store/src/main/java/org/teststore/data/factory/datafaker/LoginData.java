7b90cdb40942481f23aa69128e0be91f4558e582
Adam Cardoso <adamjuniorcardoso@gmail.com>
2024-03-15 22:57:15 -0300

    test: WishListTest

--- test-store/src/main/java/org/teststore/data/factory/datafaker/LoginData.java
+++ test-store/src/main/java/org/teststore/data/factory/datafaker/LoginData.java
@@ -1,15 +1,19 @@
 package org.teststore.data.factory.datafaker;
 
 import org.teststore.data.dto.LoginDTO;
+import org.teststore.util.ConfigProperties;
 import org.teststore.util.DataFakerGenerator;
 
 public class LoginData {
     DataFakerGenerator dataFakerGenerator = new DataFakerGenerator();
 
+    private static final String USER_PROPERTY = "User";
+    private static final String PASSWORD_PROPERTY = "Password";
+
     public LoginDTO loginDadosValidos(){
         LoginDTO loginDTO = new LoginDTO();
-        loginDTO.setEmail("zohalasyjy@mailinator.com");
-        loginDTO.setSenha("Pa$$w0rd!");
+        loginDTO.setEmail(ConfigProperties.getProperties().getProperty(USER_PROPERTY));
+        loginDTO.setSenha(ConfigProperties.getProperties().getProperty(PASSWORD_PROPERTY));
 
         return loginDTO;
     }
--- test-store/src/test/java/org/teststore/page/home/HomePage.java
+++ test-store/src/test/java/org/teststore/page/home/HomePage.java
@@ -7,7 +7,6 @@ public class HomePage extends BasePage {
     private static final By btnSignin = By.cssSelector("#_desktop_user_info > div > a > span");
     private static final By btnLogout = By.cssSelector(".hidden-sm-down.logout");
 
-
     public void clicarNoBtnSignin() {
         clicar(btnSignin);
     }
